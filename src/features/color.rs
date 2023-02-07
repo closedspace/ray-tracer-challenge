4c400f8478b8ef6a483cf4ce118fd08f04a17c35
Emil Momchilov <emil@pernik.org>
2023-02-07 06:09:29 +0200

    add a color interface

---
+++ src/features/color.rs
@@ -0,0 +1,123 @@
+// implement a color tuple type
+// pub mod color;
+//
+
+use std::ops::{Mul, Sub, Add};
+
+use super::{util::almost_equal, tuple::Tuple};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Color {
+    pub red: f64,
+    pub green: f64,
+    pub blue: f64,
+}
+
+impl Color {
+    pub fn new(red: f64, green: f64, blue: f64) -> Self {
+        Self { red, green, blue }
+    }
+
+    pub fn from_tuple(tuple: Tuple) -> Self {
+        Self {
+            red: tuple.x,
+            green: tuple.y,
+            blue: tuple.z,
+        }
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Self) -> bool {
+        almost_equal(self.red, other.red)
+            && almost_equal(self.green, other.green)
+            && almost_equal(self.blue, other.blue)
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            red: self.red + other.red,
+            green: self.green + other.green,
+            blue: self.blue + other.blue,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            red: self.red - other.red,
+            green: self.green - other.green,
+            blue: self.blue - other.blue,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Self;
+    fn mul(self, scalar: f64) -> Self {
+        Self {
+            red: self.red * scalar,
+            green: self.green * scalar,
+            blue: self.blue * scalar,
+        }
+    }
+}
+
+impl Mul for Color {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        Self {
+            red: self.red * other.red,
+            green: self.green * other.green,
+            blue: self.blue * other.blue,
+        }
+    }
+}
+
+// add tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn colors_are_tuples() {
+        let c = Color::new(-0.5, 0.4, 1.7);
+        assert_eq!(c.red, -0.5);
+        assert_eq!(c.green, 0.4);
+        assert_eq!(c.blue, 1.7);
+    }
+
+    #[test]
+    fn add_colors() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        assert_eq!(c1 + c2, Color::new(1.6, 0.7, 1.0));
+    }
+
+    #[test]
+    fn subtract_colors() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        assert_eq!(c1 - c2, Color::new(0.2, 0.5, 0.5));
+    }
+
+    #[test]
+    fn multiply_color_by_scalar() {
+        let c = Color::new(0.2, 0.3, 0.4);
+        assert_eq!(c * 2.0, Color::new(0.4, 0.6, 0.8));
+    }
+
+    // test hadamard product
+    #[test]
+    fn multiply_colors() {
+        let c1 = Color::new(1.0, 0.2, 0.4);
+        let c2 = Color::new(0.9, 1.0, 0.1);
+        assert_eq!(c1 * c2, Color::new(0.9, 0.2, 0.04));
+    }
+}
